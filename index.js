621f81ea0bdabeee7c98a5a5f0da57688277e0ac
Amir-Kermanshahani <amir.kermanshahani.bussiness@gmail.com>
2023-09-10 10:35:05 +0330

    Project Completed v1.0.0

--- index.js
+++ index.js
@@ -24,7 +24,28 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:dateString", function(req, res) {
+  const dateString = req.params.dateString;
+  let date;
 
+  if (!dateString) {
+    date = new Date();
+  } else {
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+
+  if (date.toString() === "Invalid Date") {
+    res.json({error: date.toString()});
+  } else {
+    res.json({
+      unix: date.getTime(), utc: date.toUTCString()
+    });
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
